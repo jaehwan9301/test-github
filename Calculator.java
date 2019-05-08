678c072459329c5bf74c530f777c8ca239154992
jjhwan9301 <nefdun@gmail.com>
2019-05-08 16:38:38 +0900

    public 수정

--- Calculator.java
+++ Calculator.java
@@ -2,10 +2,10 @@ package com.mycompany.test;
 public class Caculator{
   public static void main(String[] args) {
   }
-  private static in sum(int a, int b){
+  public static in sum(int a, int b){
     return a+b;
   }
-  private static in minus(int a, int b){
+  public static in minus(int a, int b){
     return a-b;
   }
 }
