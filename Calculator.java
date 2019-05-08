fea880294384854cfc37efa55ea357a46f7b2854
jjhwan9301 <nefdun@gmail.com>
2019-05-08 16:37:28 +0900

    minus 함수 추가

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,11 @@
 package com.mycompany.test;
 public class Caculator{
   public static void main(String[] args) {
-    
   }
   private static in sum(int a, int b){
     return a+b;
+  }
+  private static in minus(int a, int b){
+    return a-b;
   }
 }
